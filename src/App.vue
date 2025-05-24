a00240a98dcaea02e136e3cfd78e92c9b77d57ee
GabHrq <207466084+GabHrq@users.noreply.github.com>
2025-05-24 15:23:13 -0300

    clean up

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   <div class="body">
     <nav><Navbar /></nav>
     <div class="container">
-      <header>
-        <h1 class="text-2xl flex justify-center font-bold mb-4">Flappy Graphic</h1>
-      </header>
       <div class="game"> <!-- O jogo -->
         <PipeGame />
       </div>
@@ -18,7 +15,7 @@
 </template>
 
 <script setup>
-import Navbar from './components/page/Navbar.vue'
+import Navbar from './components/parts/Navbar.vue'
 import PipeGame from './components/PipeGame.vue'
 </script>
 
@@ -33,20 +30,8 @@ import PipeGame from './components/PipeGame.vue'
 }
 
 nav {
-  position: fixed;
-
-  margin: 0;
-  width: 100%;
   background-color: aqua;
-  justify-content: center;
-  padding: 8px 0;
-}
-
-.nav-container {
-  display: flex;
-  min-width: 50%;
-  width: 90%;
-  justify-content: space-between;
+  padding: 0.4rem 0;
 }
 
 .container {
